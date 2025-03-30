ae77f195b719ea23b2fb39d10d1c75ce59b2a2b1
al-fahad-bd <fahad.bauet@gmail.com>
2025-03-30 23:30:14 +0600

    First commit

--- android/app/src/main/kotlin/com/example/uip_tv/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/uip_tv/MainActivity.kt
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.example.uip_tv
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
-=======
-package com.example.uip_tv
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
->>>>>>> 1e03f75 (First commit)
