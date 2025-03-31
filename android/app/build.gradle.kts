aaa556d70cb12808132ccc1c0a8ec8f27fdf3caf
al-fahad-bd <fahad.bauet@gmail.com>
2025-03-31 23:25:18 +0600

    android bug fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        ndkVersion = "27.0.12077973"
     }
 
     buildTypes {
